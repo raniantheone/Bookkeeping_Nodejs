8a8678b87fdba6b1608f8bc7e59039db6e1b5775
ranian <raniantheone@gmail.com>
2018-03-23 10:50:54 +0800

    working on authen

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var cookieParser = require('cookie-parser')
 var flowRouter = require("./routes/flow");
 var configRouter = require("./routes/config");
 var analysisRouter = require("./routes/analysis");
@@ -10,9 +11,22 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use("/bookkeeping", function(req, res, next) {
+  let cookie = req.cookies.testAuth;
+  console.log("hit authen check, cookie content: " + cookie);
+  if(cookie) {
+    next();
+  }else{
+    res.cookie("testAuth", "checked");
+    next();
+  }
+});
+
+app.use("/bookkeeping", express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => res.send('Hello World!'));
 
 app.use("/flow", flowRouter);
 
